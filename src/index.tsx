f294b124f7fe7a841d1afc55d78c93e4494d89d7
hayate4th <dia.hayayer@gmail.com>
2019-10-20 22:33:18 +0900

    Test commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello, world!!!!</div>, document.getElementById('root'));
+ReactDOM.render(<div>Hello, 
+  world!!!!</div>, document.getElementById('root'));
